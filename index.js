67df80ff8a1670766568b26b924caef472a8a009
Orlando <orlandozepeda20@gmail.com>
2022-10-26 00:01:13 -0700

    added to index.js

--- index.js
+++ index.js
@@ -2,6 +2,9 @@ const inquirer = require('inquirer');
 const fs =  require('fs');
 
 
+
+const workGroup = [];
+
 function startUp() {
     inquirer.prompt([
         {
@@ -10,6 +13,14 @@ function startUp() {
             type: 'confirm',
         },
     ])
+    .then((res, err) => {
+        if (err) console.error(err);
+        if (res.startApp) {
+            addManager();
+        } else {
+            process.exit();
+        }
+    });
 }
 
 function addManager() {
@@ -76,5 +87,141 @@ function addManager() {
             type: 'confirm'
         },
     ])
-    
+
 }
+
+function addEngineer() {
+    inquirer.prompt([
+        {
+            name: 'name',
+            message: "Enter the Engineer's name:",
+            type: 'input',
+            validate: (input) => {
+                if (input){
+                    return true;
+                }
+                else {
+                    console.log("Please add the Engineer's name!");
+                    return false;
+                }
+            },
+        },
+        {
+            name: 'name',
+            message: "Enter the Engineer's id:",
+            type: 'input',
+            validate: (input) => {
+                if (input){
+                    return true;
+                }
+                else {
+                    console.log("Please add an Engineer's id!");
+                    return false;
+                }
+            },
+        },
+        {
+            name: 'name',
+            message: "Enter the Engineer's office number:",
+            type: 'input',
+            validate: (input) => {
+                if (input){
+                    return true;
+                }
+                else {
+                    console.log("Please add the Engineer's office number!");
+                    return false;
+                }
+            },
+        },
+        {
+            name: 'name',
+            message: "Enter the Engineer's email address:",
+            type: 'input',
+            validate: (input) => {
+                if (input){
+                    return true;
+                }
+                else {
+                    console.log("Please add a Engineer's email address!");
+                    return false;
+                }
+            },
+        },
+        {
+            name: 'addEmployee',
+            message: "Want to add another employee?",
+            type: 'confirm'
+        },
+    ])
+
+}
+
+function addIntern() {
+    inquirer.prompt([
+        {
+            name: 'name',
+            message: "Enter the Intern's name:",
+            type: 'input',
+            validate: (input) => {
+                if (input){
+                    return true;
+                }
+                else {
+                    console.log("Please add the Intern's name!");
+                    return false;
+                }
+            },
+        },
+        {
+            name: 'name',
+            message: "Enter the Intern's id:",
+            type: 'input',
+            validate: (input) => {
+                if (input){
+                    return true;
+                }
+                else {
+                    console.log("Please add an Intern's id!");
+                    return false;
+                }
+            },
+        },
+        {
+            name: 'name',
+            message: "Enter the Intern's office number:",
+            type: 'input',
+            validate: (input) => {
+                if (input){
+                    return true;
+                }
+                else {
+                    console.log("Please add the Intern's office number!");
+                    return false;
+                }
+            },
+        },
+        {
+            name: 'name',
+            message: "Enter the Intern's email address:",
+            type: 'input',
+            validate: (input) => {
+                if (input){
+                    return true;
+                }
+                else {
+                    console.log("Please add a Intern's email address!");
+                    return false;
+                }
+            },
+        },
+        {
+            name: 'addEmployee',
+            message: "Want to add another employee?",
+            type: 'confirm'
+        },
+    ])
+
+}
+
+startUp();
